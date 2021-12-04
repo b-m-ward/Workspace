d2a7c81588fccfe0b062dee9b7986372716b49a4
b-m-ward <bryantmward@gmail.com>
2021-12-03 22:56:24 -0500

    unzipper script

---
+++ extract_all_ps.ps1
@@ -0,0 +1,15 @@
+﻿$zipFolder = 'C:\Users\bbmmw\Desktop\music'
+
+$zipFiles = Get-ChildItem $zipFolder -Filter *.zip
+
+foreach($zipFile in $zipFiles) {
+
+    Write-Output $zipFile
+}
+
+foreach($zipFile in $zipFiles) {
+
+    $zipOutPutFolderExtended = $zipFolder + "\" + $zipFile.BaseName
+    Expand-Archive -Path $zipFile.FullName -DestinationPath $zipOutPutFolderExtended
+    Write-Output 'EXTRACTED ' + $zipFile.FullName
+}
